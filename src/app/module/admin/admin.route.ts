bb2f0b903dedf3033878874fe04b99e4eaf6f061
Alamin Mustafa Rahim <asaduzzamanalamin112809@gmail.com>
2026-05-09 17:32:09 +0600

    refector middlware

--- src/app/module/admin/admin.route.ts
+++ src/app/module/admin/admin.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { cheakAuth } from "../../midddlware/cheakAuth";
 import { Role } from "../../../generated/prisma/enums";
 import { adminController } from "./admin.controller";
+import { rateLimitMiddleware } from "../../midddlware/rateLimiter";
 
 const router = Router();
 router.get("/users", cheakAuth(Role.ADMIN), adminController.getAllUsersByAdmin);
@@ -12,21 +13,25 @@ router.get(
 );
 router.get(
   "/users/:userId",
+  rateLimitMiddleware,
   cheakAuth(Role.ADMIN),
   adminController.getOneUserByAdmin,
 );
 router.get(
   "/dashboard/stats",
+  rateLimitMiddleware,
   cheakAuth(Role.ADMIN),
   adminController.getAdminDashboardStats,
 );
 router.patch(
   "/users/role/:userId",
+  rateLimitMiddleware,
   cheakAuth(Role.ADMIN),
   adminController.updateUserRoleByAdmin,
 );
 router.delete(
   "/users/delete/:userId",
+  rateLimitMiddleware,
   cheakAuth(Role.ADMIN),
   adminController.hardDeleteUserByAdmin,
 );
