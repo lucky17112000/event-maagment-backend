869dc5e7c67750e46483f2b7de180e00af088173
Alamin Mustafa Rahim <asaduzzamanalamin112809@gmail.com>
2026-05-09 17:40:59 +0600

    rate limit increase

--- src/app/midddlware/rateLimiter.ts
+++ src/app/midddlware/rateLimiter.ts
@@ -6,7 +6,7 @@ const redis = Redis.fromEnv();
 //difrent limiter for diffrent routes
 const apiLimiter = new Ratelimit({
   redis,
-  limiter: Ratelimit.slidingWindow(10, "10 s"),
+  limiter: Ratelimit.slidingWindow(50, "10 s"),
   analytics: true,
 });
 //middlware to use the limiter
